c4ae80f16a7ce656b582fb08a360fb828d24a706
Daphne <a.milky.sky@gmail.com>
2018-03-29 14:40:37 -0700

    Modify app.use csp directives

--- server/app.js
+++ server/app.js
@@ -9,38 +9,6 @@ module.exports.app = app
 
 const csp = require(`helmet-csp`)
 
-function getDirectives () {
-  const self = `'self'`
-  const unsafeInline = `'unsafe-inline'`
-  const styles = [
-    `//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css`,
-    `//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css`
-  ]
-  const fonts = [
-    `//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css`
-  ]
-  const images = [
-    `https:`,
-    `data:`
-  ]
-  const connect = [
-    `https://stream.twitter.com/`
-  ]
-
-  return {
-    defaultSrc: [self],
-    scriptSrc: [self],
-    styleSrc: [self, unsafeInline, ...styles],
-    fontSrc: [self, ...fonts],
-    frameSrc: [self],
-    connectSrc: [self],
-    imgSrc: [self, ...images],
-    objectSrc: [self],
-    reportUri: `/api/csp/report`
-  }
-}
-
-
 app.use(middleware.morgan('dev'))
 app.use(middleware.cookieParser())
 app.use(middleware.bodyParser.urlencoded({extended: false}))
@@ -48,7 +16,26 @@ app.use(middleware.bodyParser.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use(csp({directives: getDirectives()}))
+app.use(csp({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ['style.com', "'unsafe-inline'"],
+    fontSrc: ["'self'"],
+    imgSrc: ['self', 'data:'],
+    sandbox: ['allow-forms', 'allow-scripts'],
+    reportUri: '/report-violation',
+    objectSrc: ["'none'"],
+    upgradeInsecureRequests: true,
+    workerSrc: false
+  },
+  loose: true,
+  reportOnly: true,
+  setAllHeaders: false,
+  disableAndroid: true,
+  browserSniff: true
+}))
+
 app.use(middleware.auth.session)
 app.use(middleware.passport.initialize())
 app.use(middleware.passport.session())
